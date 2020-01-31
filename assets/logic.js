da9a97ac621f388fc09c9074acad9cba17aceb07
James Murry <jmurry2014@gmail.com>
2020-01-31 16:25:07 -0600

    added modal for the search button and addded some logic to help capture what i need from the api

--- assets/logic.js
+++ assets/logic.js
@@ -1,15 +1,45 @@
 // calling the farmers market api
-var artist = 'Chance The Rapper'
-var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=test";
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-
-    // Printing the entire object to console
-    console.log(response);
-
-});
 
 
+$("#artistButton").on('click', function () {
+    var artist = $("#artistSearch").val().trim()
+    console.log(artist)
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=test&date=upcoming" + artist + "?app_id=test";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var imageURL = response[0].artist.image_url;
+        var image = $("<img id='pictureSize'>")
+        var artistName = response[0].artist.name
+
+        console.log(response)
+
+
+
+        for (var i = 0; i < 5; i++) {
+
+            console.log(response[i])
+
+
+
+        }
+
+
+        $("#artistName").html(artistName)
+        image.attr('src', imageURL)
+        $("#artistDiv").append(image)
+
+    });
+})
+
+
+
+
+
+
+$("#close").on('click', function () {
+    $("#artistDiv").empty()
+})
 
