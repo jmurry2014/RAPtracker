55a1a28b82feec74a0d520b0a1d89435638a7a48
James Murry <jmurry2014@gmail.com>
2020-02-07 19:32:28 -0600

    added google maps api and brought the map to show.html

--- assets/logic.js
+++ assets/logic.js
@@ -1,7 +1,23 @@
 $("#artistButton").on('click', function () {
     var artist = $("#artistSearch").val().trim()
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=test&date=upcoming" + artist + "?app_id=test";
-    console.log(artist)
+    var test = $("#map").attr('data-vision')
+
+    console.log(test)
+
+    if (test === 'hide') {
+        console.log('this is hidden')
+        $('#map').hide()
+        $("#map").attr('data-vision', 'show')
+
+    }
+
+
+    else {
+        console.log('this is visible')
+        $('#map').show()
+        $("#map").attr('data-vision', 'hide')
+    }
 
 
     $.ajax({
@@ -9,25 +25,17 @@ $("#artistButton").on('click', function () {
         method: "GET"
     }).then(function (response) {
         console.log(response)
-
-
         var imageURL = response[0].artist.image_url;
         var image = $("<img id='pictureSize'>")
         var artistName = response[0].artist.name
-
-
         var todayDate = new Date()
         var today = new Date(todayDate);
-        console.log('Todays Date: ' + today)
-
-
 
 
         $("#artistName").html(artistName)
         image.attr('src', imageURL)
         $("#artistImage").html(image)
         $("#venue").html(response[125].venue.name)
-
 
 
 
@@ -38,35 +46,21 @@ $("#artistButton").on('click', function () {
             var city = response[i].venue.city;
             var region = response[i].venue.region
 
-            console.log('Concert Date: ' + concertDate)
-            // if (i == 5) {
-            //     break
-            // }
 
             if (today.getTime() < concertDate.getTime()) {
-                //date 1 is newer
-                console.log('date is valid')
                 $(".tableData").before($("<tr><td>" + venueName + "</td>" + "<td>" + city + ', ' + region + "</td>" + "<td>" + time + "</td></tr>"))
             } else {
-                console.log('date is invalid')
                 continue;
-
             }
 
-
-
-
-
         }
+        $("#map").show()
 
 
 
 
     });
-
     $("#concertInfo").find("td").remove();
-
-
 
 })
 
@@ -75,3 +69,13 @@ $("#artistButton").on('click', function () {
 
 
 
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: {
+            lat: 38.899,
+            lng: -94.725
+        }
+    });
+
+} $("#map").hide()
